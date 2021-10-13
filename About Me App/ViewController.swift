0ebe9a3437914873c1709892dba1d747381489c3
Alexandr <zhenuhin@inbox.ru>
2021-10-13 20:28:07 +0300

    Initial Commit

---
+++ About Me App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me App
+//
+//  Created by Александр Женухин on 13.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
